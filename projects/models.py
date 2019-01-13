0c68043b270f66ad56c3ef921677b1d647e56184
PetarPeychev <petarpeychev98@gmail.com>
2019-01-13 12:49:05 +0000

    added Project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import json
 
-# Create your models here.
+class Project(models.Model):
+
+    # Importance of the project used for sorting
+    importance = models.PositiveSmallIntegerField()
+
+    # Visual representation of the project
+    image = models.ImageField(upload_to = 'images/projects/')
+
+    # Short summary of the project on the main page
+    summary = models.CharField(max_length = 128)
+
+    # Long description of the project when clicked
+    description = models.TextField()
+
+    # List of technologies used, stored as a JSON string in the DB
+    technologies = models.CharField(max_length = 256)
+
+    def set_technologies(self, list):
+        self.technologies = json.dumps(list)
+
+    def get_technologies(self):
+        return json.loads(self.technologies)
+
+    # Link to project (generally it's a github repo, but could be different)
+    link = models.URLField()
